c0c2522243253b104de56ed498e8efb5b7b04f53
RaiyanSohel-byte <afnan19262@gmail.com>
2025-12-13 07:03:18 +0600

    scroll to top added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "../components/Shared/Navbar";
 import Footer from "../components/Shared/Footer";
+import ScrollToTop from "../components/Shared/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <div className="bg-[#F3F5F0]">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
