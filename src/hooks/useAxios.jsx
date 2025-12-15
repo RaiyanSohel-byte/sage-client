fd11560b3d1aec7ea74ecb700610e96215f865c9
RaiyanSohel-byte <afnan19262@gmail.com>
2025-12-15 11:43:13 +0600

    main url added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React from "react";
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://sage-server-seven.vercel.app",
 });
 const useAxios = () => {
   return instance;
